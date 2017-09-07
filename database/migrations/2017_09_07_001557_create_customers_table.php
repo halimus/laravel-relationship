8fce14c62f46fdbb5a1c16cf3cd209bb1e962883
workstation\Halim <halim.webdev@gmail.com>
2017-09-06 20:46:40 -0400

    create db migration

---
+++ database/migrations/2017_09_07_001557_create_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name', 25);
+            $table->string('email', 45)->unique();
+            $table->string('phone', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('customers');
+    }
+
+}
---
+++ database/migrations/2017_09_07_003350_create_orders_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersProductsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('products', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            //$table->increments('id');
+            $table->primary(['order_id', 'product_id']); 
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->tinyInteger('quantity')->default(1);
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('orders_products');
+    }
+
+}
